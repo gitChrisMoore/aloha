2b4f0127449f492aadd4dde02f6df22fde27ab32
Chris Moore <chrismoore@Chriss-MacBook-Air.local>
2021-12-06 23:22:29 -0500

    fixed create status

--- src/providers/Transactions.js
+++ src/providers/Transactions.js
@@ -2,7 +2,7 @@ import { supabase } from '../supabase'
 import {v4 as uuid} from 'uuid'
 
 
-async function makeTx(oldTransaction, status) {
+async function makeTx(oldTransaction, status, transaction_detail) {
         
     let tx = oldTransaction
 
@@ -13,6 +13,16 @@ async function makeTx(oldTransaction, status) {
     } else if (status === 'APPROVED') {
         tx.transaction_state = 1500;
         tx.status = 'APPROVED';
+    } else if (status === 'FINAL') {
+        tx.transaction_state = 2500;
+        tx.status = 'FINAL';
+    } else if (status === 'NON_SUFFICIENT_FUNDS') {
+        tx.transaction_state = 2000;
+        tx.status = 'NON_SUFFICIENT_FUNDS';
+    }
+    
+    if (transaction_detail) {
+        tx.transaction_detail = transaction_detail
     }
 
     delete tx.id
@@ -53,20 +63,94 @@ export const Transactions = () => {
 
     const createApproved = async (oldTransaction) => {
 
-        const txState = await getStateByID(oldTransaction);
+        const priorTx = await getStateByID(oldTransaction);
 
 
-        if (txState.transaction_state === 500) { //Make Sure its in prior state
+        if (priorTx.transaction_state === 500) { //Make Sure its in prior state
             
-            console.log(txState)
+            // console.log(priorTx)
             const tx = await makeTx(oldTransaction, 'APPROVED');
             if (tx) {
+                const udpated = await updateToProcessed(priorTx);
                 const res = await createTx(tx);
-                return res
+                if(udpated && res) {
+                    return res
+                }else {
+                    console.log('createApproved Failed')
+                }
             }
         } else {
-            console.log('Transaction state error - ', txState)
+            console.log('Transaction state error - ', priorTx)
             return 
+        }
+        
+    };
+
+    const createFinal = async (currentTx, transaction_detail) => {
+
+        const priorTx = await getStateByID(currentTx);
+        if (priorTx.transaction_state === 1500) { //Make Sure its in prior state
+            
+            
+
+            const tx = await makeTx(currentTx, 'FINAL', transaction_detail);
+            if (tx) {
+                
+                const udpated = await updateToProcessed(priorTx);
+                const res = await createTx(tx);
+                if(udpated && res) {
+                    return res
+                }else {
+                    console.log('Create Final Failed')
+                }
+                
+            }
+        } else {
+            console.log('Transaction state error - ', priorTx)
+            return 
+        }
+        
+    };
+
+    const createNonSufficientFunds = async (currentTx, transaction_detail) => {
+
+        const priorTx = await getStateByID(currentTx);
+        if (priorTx.transaction_state === 1500) { //Make Sure its in prior state
+            
+            
+
+            const tx = await makeTx(currentTx, 'NON_SUFFICIENT_FUNDS', transaction_detail);
+            if (tx) {
+                
+                const udpated = await updateToProcessed(priorTx);
+                const res = await createTx(tx);
+                if(udpated && res) {
+                    return res
+                }else {
+                    console.log('ERROR - createNonSufficientFunds')
+                }
+                
+            }
+        } else {
+            console.log('Transaction state error - ', priorTx)
+            return 
+        }
+        
+    };
+
+    const updateToProcessed = async (tx) => {
+        
+        let {data, error} = await supabase
+            .from("transactions")
+            .update({ processed: true})
+            .match({ id: tx.id })
+            .select("*")
+            .eq('id', tx.id)
+            .limit(1);
+        if (error) console.log('error - ', error)
+        if (data.length > 0) {
+            // console.log("found data, ", data)
+            return data[0]
         }
         
     };
@@ -107,6 +191,7 @@ export const Transactions = () => {
             .from('transactions')
             .select("*")
             .eq('status', status)
+            .eq('processed', false)
             .order("id", { ascending: false })
         if (error) console.log('error - ', error)
         if (data.length > 0) {
@@ -116,11 +201,36 @@ export const Transactions = () => {
         }
     };
 
+    const getApprovedTransactions = async () => {
+
+        const res = await getTransactionsByStatus('APPROVED');
+        return res
+    };
+
+    const getCurrentBalanceByUserID = async (id) => {
+        let {data, error} = await supabase
+            .from('current_balance')
+            .select("*")
+            .eq('user_id', id)
+            .single()
+        if (error) console.log('error - ', error)
+        if (data) {
+            return data
+        } else {
+            return null
+        }
+
+    };
+
     return {
         createPendingApproval,
         createApproved,
         getTransactionsByStatus,
-        getByID
+        getCurrentBalanceByUserID,
+        getByID,
+        getApprovedTransactions,
+        createFinal,
+        createNonSufficientFunds
     }
 
 }
